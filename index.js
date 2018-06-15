16b47ff88cd2dcf4908ce0c28f1fcfd4f2794e27
Nicolas Gomez <ngomez@np6.com>
2018-06-15 12:02:59 +0200

    Not so simple now ...

--- index.js
+++ index.js
@@ -1,66 +1,104 @@
-const crypto = require('crypto');
-const fs = require("fs");
+const crypto = require('crypto')
+const fs = require('fs')
 
-const defaultConfiguration = {};
+const defaultConfiguration = {}
 
-function encrypt(text, encryptKey, algorithm) {
+function checksum (filepath, algorithm) {
+  return new Promise((resolve, reject) => {
+    let sum = crypto.createHash(algorithm)
+    fs.createReadStream(filepath)
+      .on('data', function (data) {
+        sum.update(data)
+      })
+      .on('end', function () {
+        resolve(sum.digest('hex'))
+      })
+      .on('error', (error) => {
+        let clearerError = new Error('Can\'t read the file to checksum')
+        clearerError.stack += '\nCaused by: ' + error.stack
+        reject(clearerError)
+      })
+  })
+}
+
+function encrypt (text, encryptKey, algorithm, ivsize) {
   // Encryption disabled
-  if(encryptKey == null) return text;
+  if (encryptKey == null) return text
 
-  var cipher = crypto.createCipher(algorithm, encryptKey);
-  var crypted = cipher.update(text,'utf8','hex');
-  crypted += cipher.final('hex');
-  return crypted;
+  let iv = crypto.randomBytes(ivsize)
+  let cipher = crypto.createCipheriv(algorithm, encryptKey, iv)
+  let crypted = cipher.update(text, 'utf8', 'hex')
+  crypted += cipher.final('hex')
+  return iv.toString('hex') + ':' + crypted
 }
 
-function decrypt(text, encryptKey, algorithm) {
+function decrypt (text, encryptKey, algorithm) {
   // Encryption disabled
-  if(encryptKey == null) return text;
+  if (encryptKey == null) return text
 
-  var decipher = crypto.createDecipher(algorithm, encryptKey);
-  var dec = decipher.update(text,'hex','utf8');
-  dec += decipher.final('utf8');
-  return dec;
+  let splitted = text.split(':')
+  let iv = Buffer.from(splitted[0], 'hex')
+  let decipher = crypto.createDecipheriv(algorithm, encryptKey, iv)
+  let dec = decipher.update(splitted[1], 'hex', 'utf8')
+  dec += decipher.final('utf8')
+  return dec
 }
 
-function readConfFromFile(filepath, encryptKey, algorithm) {
-  if(!fs.existsSync(filepath))
-    return { needStore: true, obj: defaultConfiguration };
-  return { needStore: false, obj: JSON.parse(decrypt(fs.readFileSync(filepath, { encoding: "utf8" }), encryptKey, algorithm)) };
+function readConfFromFile (filepath, encryptKey, algorithm) {
+  if (!fs.existsSync(filepath)) { return { needStore: true, obj: defaultConfiguration } }
+  return { needStore: false, obj: JSON.parse(decrypt(fs.readFileSync(filepath, { encoding: 'utf8' }), encryptKey, algorithm)) }
 }
 
-function storeConfToFile(filepath, obj, encryptKey, algorithm)
-{ fs.writeFileSync(filepath, encrypt(JSON.stringify(obj), encryptKey, algorithm)); }
+function storeConfToFile (filepath, obj, encryptKey, algorithm, ivsize) { fs.writeFileSync(filepath, encrypt(JSON.stringify(obj), encryptKey, algorithm, ivsize)) }
 
-module.exports = class ConfigurationHandler {
-  constructor(filepath, encryptKey = null, algorithm = 'aes-256-ctr') {
-    this.internalConfObj = null;
-    this.encryptKey = encryptKey;
-    this.algorithm = algorithm;
-    this.filepath = filepath;
-    this.needStore = false;
+module.exports = class Configurator {
+  constructor (filepath, encryptKey = null, checksum = 'md5', algorithm = 'aes-256-ctr', ivsize = 16) {
+    this.checksumAlgorithm = checksum
+    this.encryptKey = encryptKey
+    this.internalConfObj = null
+    this.algorithm = algorithm
+    this.filepath = filepath
+    this.needStore = false
+    this.checksum = null
+    this.ivsize = ivsize
   }
 
-  get(key) {
+  async get (key) {
     // Lazy initialize configuration
-    if(this.internalConfObj == null) {
-      var res = readConfFromFile(this.filepath, this.encryptKey, this.algorithm);
-      this.internalConfObj = res.obj;
-      this.needStore = res.needStore;
+    if (!this.__init()) {
+      try {
+        let sum = await checksum(this.filepath, this.checksumAlgorithm)
+        if (this.checksum === null) {
+          this.checksum = sum
+        } else if (this.checksum !== sum) {
+          this.internalConfObj = readConfFromFile(this.filepath, this.encryptKey, this.algorithm).obj
+        }
+      } catch (err) {
+        console.warn('Can\'t checksum the configuration file, check that the file hasn\'t been removed and that your system supports "' + this.checksumAlgorithm + '"', err)
+      }
     }
-    return this.internalConfObj[key];
+    return this.internalConfObj[key]
   }
 
-  set(key, value) {
+  set (key, value) {
+    this.__init()
+    this.internalConfObj[key] = value
+    this.needStore = true
+  }
+
+  persist () {
+    if (this.needStore) { storeConfToFile(this.filepath, this.internalConfObj, this.encryptKey, this.algorithm, this.ivsize) }
+  }
+
+  __init () {
     // Lazy initialize configuration
-    if(this.internalConfObj == null)
-      this.internalConfObj = readConfFromFile(this.filepath, this.encryptKey, this.algorithm).obj;
-    this.internalConfObj[key] = value;
-    this.needStore = true;
-  }
-
-  persist() {
-    if(this.needStore)
-      storeConfToFile(this.filepath, this.internalConfObj, this.encryptKey, this.algorithm);
+    if (this.internalConfObj == null) {
+      var res = readConfFromFile(this.filepath, this.encryptKey, this.algorithm)
+      this.internalConfObj = res.obj
+      this.needStore = res.needStore
+      this.persist()
+      return true
+    }
+    return false
   }
 }
--- test/tests.js
+++ test/tests.js
@@ -1,68 +1,97 @@
-const ConfigurationHandler = require("../index.js");
-const assert = require('assert');
-const path = require('path');
-const fs = require('fs');
+/* eslint-env mocha */
 
-const UnencryptedFilePath = path.join(__dirname, 'test-unencrypted.conf');
-const EncryptedFilePath = path.join(__dirname, 'test-encrypted.conf');
-var UnencryptedConfigurationHandler = new ConfigurationHandler(UnencryptedFilePath);
-var EncryptedConfigurationHandler = new ConfigurationHandler(EncryptedFilePath, "3a6b3b5d594a277234654964542f2e29414d5d5938412f564424563927");
+const MongoBackend = require('../Backends/Mongo.js')
+const FsBackend = require('../Backends/Filesystem.js')
+const ConfigurationHandler = require('../index.js')
+const replace = require('replace-in-file')
+const assert = require('assert')
+const path = require('path')
+const fs = require('fs')
 
-describe('ConfigurationHandler', function() {
-  after(function() {
-    fs.unlinkSync(UnencryptedFilePath);
-    fs.unlinkSync(EncryptedFilePath);
-  });
+const UnencryptedFilePath = path.join(__dirname, 'test-unencrypted.conf')
+const EncryptedFilePath = path.join(__dirname, 'test-encrypted.conf')
+var UnencryptedConfigurationHandler = new ConfigurationHandler(UnencryptedFilePath)
+var EncryptedConfigurationHandler = new ConfigurationHandler(EncryptedFilePath, '79756c6a23666b4f584d225a2b7d3a43')
 
-  describe('Unencrypted', function() {
-    describe('# set()', function() {
-      it('should correctly set a retrievable value to the store', function() {
-        UnencryptedConfigurationHandler.set("testKey", "testValue"); // Set a key/value pair
-        assert.equal("testValue", UnencryptedConfigurationHandler.get("testKey")); // Retrieve it
-      });
-      it('should correctly retrieve value previously persisted to the filesystem', function() {
-        UnencryptedConfigurationHandler.set("testPersistentUnencryptedKey", "testPersistentEncryptedValue"); // Set a key/value pair
-        UnencryptedConfigurationHandler.persist(); // Store to disk
+describe('ConfigurationHandler', function () {
+  after(function () {
+    fs.unlinkSync(UnencryptedFilePath)
+    fs.unlinkSync(EncryptedFilePath)
+  })
+
+  describe('Backends', function () {
+    describe('# Mongo', function () {
+      describe('# store()', function () {
+        it('should correctly store a file', async function () {
+          let mb = new MongoBackend('test', 'Configuration')
+          await mb.store('Tarasta', fs.createReadStream('LICENSE'))
+          await mb.retrieve('Tarasta', fs.createWriteStream('LICENSE-1'))
+        })
+      })
+    })
+  })
+
+  describe('Unencrypted', function () {
+    describe('# set()', function () {
+      it('should correctly set a retrievable value to the store', async function () {
+        UnencryptedConfigurationHandler.set('testKey', 'testValue') // Set a key/value pair
+        assert.equal('testValue', await UnencryptedConfigurationHandler.get('testKey')) // Retrieve it
+      })
+      it('should correctly retrieve value previously persisted to the filesystem', async function () {
+        UnencryptedConfigurationHandler.set('testPersistentUnencryptedKey', 'testPersistentUnencryptedValue') // Set a key/value pair
+        UnencryptedConfigurationHandler.persist() // Store to disk
         var testUnencryptedConfigurationHandler =
               new ConfigurationHandler(path.join(__dirname, 'test-unencrypted.conf'))
-        assert.equal("testPersistentEncryptedValue", testUnencryptedConfigurationHandler.get("testPersistentUnencryptedKey")); // Retrieve it
-      });
-    });
-    describe('# configFile()', function() {
-      it('should exists after a call to persist, even if it\'s empty', function() {
-        UnencryptedConfigurationHandler.persist(); // Store to disk
-        assert.ok(fs.existsSync(UnencryptedFilePath)); // Is conf file existsing
-      });
-      it('should be a valid JSON file', function() {
-        UnencryptedConfigurationHandler.persist(); // Store to disk
-        assert.doesNotThrow(() => JSON.parse(fs.readFileSync(UnencryptedFilePath, { encoding: "utf8" })));
-      });
-    });
-  });
+        assert.equal('testPersistentUnencryptedValue', await testUnencryptedConfigurationHandler.get('testPersistentUnencryptedKey')) // Retrieve it
+      })
+    })
+    describe('# configFile()', function () {
+      it('should exists after a call to persist, even if it\'s empty', function () {
+        UnencryptedConfigurationHandler.persist() // Store to disk
+        assert.ok(fs.existsSync(UnencryptedFilePath)) // Is conf file existsing
+      })
+      it('should be a valid JSON file', function () {
+        UnencryptedConfigurationHandler.set('testKey', 'testValue') // Set a key/value pair
+        UnencryptedConfigurationHandler.persist() // Store to disk
+        assert.doesNotThrow(() => JSON.parse(fs.readFileSync(UnencryptedFilePath, { encoding: 'utf8' })))
+      })
+      it('should be reloaded when changed and a configuration key is asked', async function () {
+        UnencryptedConfigurationHandler.set('testKey', 'testValue') // Set a key/value pair
+        UnencryptedConfigurationHandler.persist() // Store to disk
+        replace.sync({
+          files: UnencryptedFilePath,
+          from: 'testValue',
+          to: 'testOverridenValue'
+        })
+        assert.equal('testOverridenValue', await UnencryptedConfigurationHandler.get('testKey'))
+      })
+    })
+  })
 
-  describe('Unencrypted', function() {
-    describe('# set()', function() {
-      it('should correctly set a retrievable value to the store', function() {
-        EncryptedConfigurationHandler.set("testKey", "testValue"); // Set a key/value pair
-        assert.equal("testValue", EncryptedConfigurationHandler.get("testKey")); // Retrieve it
-      });
-      it('should correctly retrieve value previously persisted to the filesystem', function() {
-        EncryptedConfigurationHandler.set("testPersistentEncryptedKey", "testPersistentEncryptedValue"); // Set a key/value pair
+  describe('Encrypted', function () {
+    describe('# set()', function () {
+      it('should correctly set a retrievable value to the store', async function () {
+        EncryptedConfigurationHandler.set('testKey', 'testValue') // Set a key/value pair
+        assert.equal('testValue', await EncryptedConfigurationHandler.get('testKey')) // Retrieve it
+      })
+      it('should correctly retrieve value previously persisted to the filesystem', async function () {
+        EncryptedConfigurationHandler.set('testPersistentEncryptedKey', 'testPersistentEncryptedValue') // Set a key/value pair
         EncryptedConfigurationHandler.persist() // Store to disk
         var testEncryptedConfigurationHandler =
-              new ConfigurationHandler(path.join(__dirname, 'test-encrypted.conf'), "3a6b3b5d594a277234654964542f2e29414d5d5938412f564424563927");
-        assert.equal("testPersistentEncryptedValue", testEncryptedConfigurationHandler.get("testPersistentEncryptedKey")); // Retrieve it
-      });
-    });
-    describe('# configFile()', function() {
-      it('should exists after a call to persist, even if it\'s empty', function() {
-        EncryptedConfigurationHandler.persist(); // Store to disk
-        assert.ok(fs.existsSync(EncryptedFilePath)); // Is conf file existsing
-      });
-      it('shouldn\'t be a valid JSON file', function() {
-        UnencryptedConfigurationHandler.persist(); // Store to disk
-        assert.throws(() => JSON.parse(fs.readFileSync(EncryptedFilePath, { encoding: "utf8" })));
-      });
-    });
-  });
-});
+              new ConfigurationHandler(path.join(__dirname, 'test-encrypted.conf'), '79756c6a23666b4f584d225a2b7d3a43')
+        assert.equal('testPersistentEncryptedValue', await testEncryptedConfigurationHandler.get('testPersistentEncryptedKey')) // Retrieve it
+      })
+    })
+    describe('# configFile()', function () {
+      it('should exists after a call to persist, even if it\'s empty', function () {
+        EncryptedConfigurationHandler.persist() // Store to disk
+        assert.ok(fs.existsSync(EncryptedFilePath)) // Is conf file existsing
+      })
+      it('shouldn\'t be a valid JSON file', function () {
+        EncryptedConfigurationHandler.set('testKey', 'testValue') // Set a key/value pair
+        EncryptedConfigurationHandler.persist() // Store to disk
+        assert.throws(() => JSON.parse(fs.readFileSync(EncryptedFilePath, { encoding: 'utf8' })))
+      })
+    })
+  })
+})
